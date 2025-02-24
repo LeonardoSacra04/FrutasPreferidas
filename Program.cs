4f8d5d78c5a46bcf116a2c123141c0701224d961
LeonardoSacra04 <oliveiraleo04@gmail.com>
2025-02-24 11:06:22 -0300

    Finaliza programa FrutasFavoritas

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine(" ###################### ");
+Console.WriteLine("### Frutas Favoritas ###");
+Console.WriteLine(" ###################### ");
+
+Console.WriteLine();
+
+Console.Write("1. ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Melancia");
+Console.ResetColor();
+
+Console.Write("2. ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Uva");
+Console.ResetColor();
+
+Console.Write("3. ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Maçã");
+Console.ResetColor();
+
+Console.Write("4. ");
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Banana");
+Console.ResetColor();
+
+Console.Write("5. ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.BackgroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Maracujá");
+Console.ResetColor();
